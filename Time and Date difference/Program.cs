b7e6dc4513269594cdb2130166f45017e2ee9426
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-12-05 15:24:17 +0200

    Add project files.

---
+++ Time and Date difference/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_and_Date_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today);
+            Console.Write("Insert date here (D/M/Y): ");
+
+
+            string sDate = Console.ReadLine();
+
+            while (!DateTime.TryParse(sDate, out today))
+            {
+                Console.WriteLine("Please enter valid date in this form (D/M/Y)");
+                sDate = Console.ReadLine();
+            }
+
+            DateTime userDate = DateTime.Parse(sDate);
+
+            TimeSpan elapsed;
+
+            elapsed = DateTime.Now.Subtract(userDate);
+
+            Console.WriteLine(Math.Abs(elapsed.TotalDays));
+
+            Console.ReadLine();
+        }
+    }
+}
